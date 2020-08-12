ef24259005b604260b0710a090ebf4a6b95ddd83
4480135 <4480135@RTS-365.dk>
2020-08-12 10:35:52 +0200

    Load events from API.

--- night-club/src/js/index.js
+++ night-club/src/js/index.js
@@ -34,16 +34,72 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 
     //-- Section 2, slider --//
-    let eventsSliderActiveDOM = document.querySelector(".events-slider-button.active");
     let eventsSliderSlide = 0;
     
     const eventsSliderDOM = document.querySelector(".events-slider");
     const eventsSliderControlsDOM = document.querySelector(".events-slider-controls");
 
+    const eventsSliderLoopFunc = () => {
+        eventsSliderActiveDOM.classList.remove("active");
+
+        eventsSliderSlide++;
+        if(eventsSliderSlide >= eventsSliderControlsDOM.children.length) eventsSliderSlide = 0;
+        eventsSliderActiveDOM = eventsSliderControlsDOM.children[eventsSliderSlide];
+
+        eventsSliderActiveDOM.classList.add("active");
+        eventsSliderDOM.style.transform = `translateX(-${eventsSliderSlide * 100}%)`;
+    };
+
+    const MONTHS = [
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ];
+
+    await fetch("http://localhost:4000/events")
+    .then((e) => e.json())
+    .then((e) => {
+        for(let i = 0; i < e.length; i++) {
+            const eventDateTimeArr = e[i].eventDate.split("T");
+            const eventDateSplitArr = eventDateTimeArr[0].split("-");
+            const eventTimeArr = eventDateTimeArr[1].split(".")[0].split(":");
+            const eventTimeHour = eventTimeArr[0];
+            const eventTimeMinute = eventTimeArr[1];
+
+            const eventsSliderItemTemplate = `
+            <div class="events-slider-item">
+                <article class="event-post">
+                    <section class="event-post-upper">
+                        <img src="/assets/img/content/reastaurant_1.jpg">
+                        <div class="event-post-cover">
+                            <a href="/book" class="event-post-book">Book Now</a>
+                        </div>
+                        <div class="event-post-info">
+                            <h2 class="event-post-title">${e[i].eventName}</h2>
+                            <p>${e[i].eventDescription}</p>
+                        </div>
+                    </section>
+                    <section class="event-post-lower">
+                        <p class="event-post-date">${eventDateSplitArr[2]} ${MONTHS[parseInt(eventDateSplitArr[1])]}</p>
+                        <p class="event-post-time">${eventTimeHour <= 12 ? eventTimeHour : eventTimeHour - 12}:${eventTimeMinute} ${eventTimeHour <= 12 ? "AM" : "PM"}</p>
+                        <p class="event-post-location">${e[i].location}</p>
+                    </section>
+                </article>
+            </div>
+            `;
+
+            eventsSliderDOM.innerHTML += eventsSliderItemTemplate;
+
+
+            if(i % 2 == 0) {
+                eventsSliderControlsDOM.innerHTML += `<div class="events-slider-button ${i / 2 == 0 ? "active" : ""}" data-val="${i / 2}"></div>`;
+            }
+        }
+    });
+
+    let eventsSliderActiveDOM = document.querySelector(".events-slider-button.active");
+
     eventsSliderControlsDOM.addEventListener("click", (e) => {
         if(e.target.classList.contains("events-slider-button")) {
             clearInterval(eventsSliderLoop);
-            eventsSliderLoop = setInterval(eventsSliderLoopFunc, 1000 * 5);    
 
             eventsSliderActiveDOM.classList.remove("active");
 
@@ -60,23 +116,8 @@ document.addEventListener("DOMContentLoaded", async () => {
         clearInterval(eventsSliderLoop);
     });
 
-    eventsSliderDOM.addEventListener("mouseleave", () => {
-        eventsSliderLoop = setInterval(eventsSliderLoopFunc, 1000 * 5);    
-    });
-
-    const eventsSliderLoopFunc = () => {
-        eventsSliderActiveDOM.classList.remove("active");
-
-        eventsSliderSlide++;
-        if(eventsSliderSlide >= eventsSliderControlsDOM.children.length) eventsSliderSlide = 0;
-        eventsSliderActiveDOM = eventsSliderControlsDOM.children[eventsSliderSlide];
-
-        eventsSliderActiveDOM.classList.add("active");
-        eventsSliderDOM.style.transform = `translateX(-${eventsSliderSlide * 100}%)`;
-    };
-
     let eventsSliderLoop = setInterval(eventsSliderLoopFunc, 1000 * 5);
-    
+
 
     //-- Finished loading --//
     finishedLoading = true;
